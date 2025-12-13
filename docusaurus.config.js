1916946d9ec7754479b462ab512decb2639ef3b6
Your Name <you@example.com>
2025-12-13 22:30:59 +0500

    Add all remaining files for full project

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://warsaiqbal.github.io/physical-ai-humanoid-robotics-textbook/',
+  url: 'https://github.com/warsaiqbal/physical-ai-textbook.git',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
